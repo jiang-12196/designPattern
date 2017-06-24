5a926dac7443b0fa59be610e0a74390edbda7aed
jiang1991 <jiang12196@126.com>
2017-06-24 22:42:35 +0800

    add protoType pattern

--- protoType.cpp
+++ protoType.cpp
@@ -21,7 +21,7 @@ class Resume : ICloneable {
 
     public void Display() {
         Console.WriteLine("{0} {1} {2}", name, sex, age);
-        Console.WriteLine("work experience {0} {1}", t;timeArea, company);
+        Console.WriteLine("work experience {0} {1}", timeArea, company);
     }
 
     public Object Clone (){
@@ -42,4 +42,62 @@ static void Main(string[] args) {
 
     jiang.Display();
     zuo.Display();
+    han.Display();
+
+    Console.Read(); 
+}
+
+//深复制
+
+class WorkExperience : ICloneable {
+    private string workDate;
+    public string WorkDate {
+        get {return workDate};
+        set { workDate = value};
+    }
+
+    private string company;
+    public string Company {
+        get {return company};
+        set {company = value};
+    }
+    
+    public Object Clone() {
+        return (Object)this.MemberwiseClone();
+    }
+}
+
+class Resume : ICloneable {
+    private string name;
+    private string sex;
+    private string age;
+    private WorkExperience work;
+
+    public Resume(string name) {
+        this.name = name;
+        work = new WorkEcperience();
+    }
+    private Resume(WorkExperience work) {
+        this.work = (WorkExperience)work.Clone();
+    }
+    public void SetPersonalInfo(string sex, string age) {
+        this.sex = sex;
+        this.age = age;
+    }
+    public void SetWorkExperience(string workDate, string company) {
+        work.WorkDate = workDate;
+        work.Company = company;
+    }
+    public void Display() {
+        Console.WriteLine("{0} {1} {2}", name, sex, age);
+        Console.WriteLine("work experience {0} {1}", work.WorkDate, work.Company);
+    }
+
+    public Object Clone(){
+        Resume obj = new Resume(this.work);
+        obj.name = this.name;
+        obj.sex = this.sex;
+        obj.age = this.age;
+        return obj;
+    }
 }
