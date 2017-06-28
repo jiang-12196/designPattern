cc590294dd5f1d97cc27699753db69525621fb3c
jiang1991 <jiang12196@126.com>
2017-06-29 00:15:07 +0800

    add builder pattern code

--- builderPattern.cpp
+++ builderPattern.cpp
@@ -61,3 +61,87 @@ pbThin.CreatePerson();
 PersonFatBuilder pfb = new PersonFatBuilder(pictureBox2.CreateGraphics(), p);
 PersonDirector pdFat = new PersonDirector(pfb);
 pdFat.CreatePerson();
+
+
+// 建造者模式
+
+//产品类， 可以是小人，也可以是食品，由多个部件组成
+class Product{
+    IList<string> parts = new List<string>();
+
+    public void Add(string part){
+        parts.Add(part);
+    }
+
+    public void Show(){
+        Console.WriteLine('产品：');
+        foreach(string part in parts){
+            Console.WriteLine(part);
+        }
+    }
+}
+
+// 抽象建造者类
+abstract class Builder{
+    public abstract void BuildPartA();
+    public abstract void BuildPartb();
+    public abstract Product GetResult();
+}
+// 具体建造者类
+class ConcreteBuilder1 : Builder{
+    private Product product = new Product();
+
+    public override void BuildPartA(){
+        product.Add('部件A');
+    }
+
+
+    public override void BuildPartB(){
+        product.Add('部件B');
+    }
+
+    public override Product GetResult(){
+        return product;
+    }
+}
+
+class ConcreteBuilder2 : Builder{
+    private Product product = new Product();
+
+    public override void BuildPartA(){
+        product.Add('部件X');
+    }
+
+
+    public override void BuildPartB(){
+        product.Add('部件Y');
+    }
+
+    public override Product GetResult(){
+        return product;
+    }
+}
+
+//指挥者类
+class Director {
+    public void Construct(Builder builder){
+        builder.BuildPartA();
+        builder.BuildPartB();
+    }
+}
+
+static void Main(string[] args){
+    Director director = new Director();
+    Builder b1 = new ConcreteBuilder1();
+    Builder b2 = new ConcreteBuilder2();
+
+    director.Construct(b1);
+    product p1 = b1.GetResult();
+    p1.Show();
+
+    director.Construct(b2);
+    product p2 = b2.GetResult();
+    p2.Show();
+
+    Console.Read();
+}
