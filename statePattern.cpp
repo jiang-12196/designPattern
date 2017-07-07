7a3f54a89b5247b304fcbc5ee1cdd1d42493af51
jiang1991 <jiang12196@126.com>
2017-07-07 23:26:13 +0800

    add state pattern

---
+++ statePattern.cpp
@@ -0,0 +1,42 @@
+abstract class State {
+    public abstract void Handle(Context context);
+}
+
+class ConcreteStateA : State {
+    public override void Handle(Context context) {
+        context.State = new ConcreteStateB();
+    }
+}
+
+class ConcreteStateB : State {
+    public override void Handle(Context context) {
+        context.State = new ConcreteStateA();
+    }
+}
+
+class Context {
+    private State state;
+    public Context(State state) {
+        this.state = state;
+    }
+    public State state{
+        get (return state;)
+            set {
+        state = value;
+        Console.WriteLine("now state"+ state.GetType().Name);
+        }
+    }
+    public void Request(){
+        state.Handle(this)
+    }
+}
+
+
+static void Main(string[] args) {
+    Context c = new Context(new ConcreteStateA());
+    c.Request();
+    c.Request();
+    c.Request();
+
+    Console.Read();
+}
