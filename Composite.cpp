03faaad04087cfee3295d0667df78faa7f590287
jiang1991 <jiang12196@126.com>
2017-07-15 23:44:00 +0800

    fix composite

--- Composite.cpp
+++ Composite.cpp
@@ -64,3 +64,95 @@ static void Main(string[] args){
     root.Display();
     Console.Read();
 }
+
+
+// actual code
+
+abstract class Company {
+    protected string name;
+
+    public Company(string name){
+        this.name = name;
+    }
+    public abstract void Add(Company c);
+    public abstract void Remove(Company c);
+    public abstract void Display(int depth);
+    public abstract void LineOfDuty();
+}
+
+class ConcreteCompany : Company {
+    private List<Company>children=new List<Company>;
+    public ConcreteCompany(string name) : base(string name){}
+    public abstract void Add(Company c) {
+        children.Add(c)
+    };
+    
+    public abstract void Remove(Company c) {
+        children.Remove(c)
+    };
+
+    public abstract void Display(int depth) {
+        Console.WriteLine(new String('-', depth) + name);
+        foreach(Component component in children) {
+            component.Display(depth + 2);
+        }
+    };
+
+    public override void LineOfDuty(){
+        foreach (Company component in children){
+            component.LineOfDuty();
+        }
+    }
+}
+
+class HRDepartment : Company {
+    public HRDepartment(string name) : base(name){}
+    public override void Add(Company c) {};
+    public override void Remove(Company c) {};
+
+    public override void Display(int depth){
+         Console.WriteLine(new String('-', depth) + name);
+    }
+
+    public override void LineOfDuty(){
+        Console.WriteLine("{0} 员工招聘培训管理". name);
+    }
+
+}
+
+
+class FinanceDepartment : Company {
+    public FinanceDepartment(string name) : base(name){}
+    public override void Add(Company c) {};
+    public override void Remove(Company c) {};
+
+    public override void Display(int depth){
+         Console.WriteLine(new String('-', depth) + name);
+    }
+
+    public override void LineOfDuty(){
+        Console.WriteLine("{0} 公司财务收支管理". name);
+    }
+}
+
+static void Main(string[] args) {
+    ConcreteCompany root = new ConcreteCompany('北京总公司');
+    root.Add(new HRDepartment('总公司人事资源部'))；
+    root.Add(new HRDepartment('总公司财务部'))；
+    
+
+    ConcreteCompany comp = new ConcreteCompany('华北分公司');
+    comp.Add(new HRDepartment('华北分公司人事资源部'))；
+    comp.Add(new HRDepartment('华北分公司财务部'))；
+    root.Add(comp)
+    
+    ConcreteCompany comp1 = new ConcreteCompany('华南分公司');
+    comp1.Add(new HRDepartment('华南分公司人事资源部'))；
+    comp1.Add(new HRDepartment('华南分公司财务部'))；
+    root.Add(comp１)
+
+    root.Display(1);
+
+    root.LineOfDuty(1);
+    Console.Read();
+}
