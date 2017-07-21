7c0605aaac0eee4157564250ca3493a014ae857b
jiang1991 <jiang12196@126.com>
2017-07-21 22:38:46 +0800

    fix

---
+++ Iterator.cpp
@@ -0,0 +1,17 @@
+abstract class Iterator {
+    public abstract object First();
+    public abstract object Next();
+    public abstract bool IsDone();
+    public abstract object CurrentItem();
+}
+
+abstract class Aggregate {
+    public abstract Iterator CreateIterator();
+}
+
+class ConcreteIterator : Iterator {
+    private ConcreteAggregate aggregate;
+    private int current = 0;
+
+    public ConcreteIterator(Con)
+}
